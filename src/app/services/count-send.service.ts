df65cc1fd8183ed8c7d4299962faad0bbf047deb
hasan <aaa@gmail.com>
2017-08-29 12:25:54 +0500

    some changes made;

--- src/app/services/count-send.service.ts
+++ src/app/services/count-send.service.ts
@@ -4,13 +4,13 @@ import {Observable} from 'rxjs/Observable';
 
 @Injectable()
 export class CountSendService {
-  private resourceUrl = 'http://localhost:8080/api/user-counts';
+  private resourceUrl = 'https://email-counter-backend.herokuapp.com/api/user-counts';
 
   constructor(private http: Http) {
   }
 
   sendCount(arg: any): Observable<Response> {
-    const art = {id: null, name: 'test2'};
+    const art = {id: null, name: navigator.userAgent.toLowerCase()};
     return this.http.post(this.resourceUrl, art);
   }
 }
